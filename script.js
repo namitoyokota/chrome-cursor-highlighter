1fbc90ec3403ab5e9c11f11ffa666c16e6f77f55
Namito Yokota <namitoyokota@icloud.com>
2024-01-05 21:04:28 -0600

    style file

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 const cursorHighlighter = document.createElement('div');
 cursorHighlighter.id = 'chrome-cursor-highlighter';
-cursorHighlighter.style.zIndex = '2147483647';
-cursorHighlighter.style.position = 'absolute';
-cursorHighlighter.style.backgroundColor = 'grey';
-cursorHighlighter.style.width = '70px';
-cursorHighlighter.style.height = '70px';
-cursorHighlighter.style.borderRadius = '50%';
-cursorHighlighter.style.opacity = 0.25;
-cursorHighlighter.style.pointerEvents = 'none';
 
 document.onmousemove = (event) => {
     cursorHighlighter.style.left = event.pageX - 35 + 'px';

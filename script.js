34224e1196ec9f4bb114acf06f4197bb13564dc4
Namito Yokota <namitoyokota@icloud.com>
2024-01-05 20:18:59 -0600

    clean up

--- script.js
+++ script.js
@@ -3,18 +3,15 @@ cursorHighlighter.id = 'chrome-cursor-highlighter';
 cursorHighlighter.style.zIndex = '2147483647';
 cursorHighlighter.style.position = 'absolute';
 cursorHighlighter.style.backgroundColor = 'grey';
-cursorHighlighter.style.width = '50px';
-cursorHighlighter.style.height = '50px';
-cursorHighlighter.style.border = '5px solid black';
+cursorHighlighter.style.width = '70px';
+cursorHighlighter.style.height = '70px';
 cursorHighlighter.style.borderRadius = '50%';
 cursorHighlighter.style.opacity = 0.25;
 cursorHighlighter.style.pointerEvents = 'none';
 
 document.onmousemove = (event) => {
-    newX = event.pageX - 25;
-    newY = event.pageY - 25;
-    cursorHighlighter.style.left = newX + 'px';
-    cursorHighlighter.style.top = newY + 'px';
+    cursorHighlighter.style.left = event.pageX - 35 + 'px';
+    cursorHighlighter.style.top = event.pageY - 35 + 'px';
 };
 
 chrome.storage.local.get('enabled', function (data) {
